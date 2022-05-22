eddb1f47f7b8bab0543bc69329b2256bc224892c
Owner Of Nano <87079094+muzrblx@users.noreply.github.com>
2022-05-22 12:11:51 -0400

    Update 4PHLA.lua

--- Hub/Loaders/4PHLA.lua
+++ Hub/Loaders/4PHLA.lua
@@ -17,6 +17,10 @@ rconsoleprint('Thanks for choosing 4PHLA | By clicking Enter you agree that if y
 
 if rconsoleinput() then
     rconsolewarn('Loading in 5 seconds')
+    wait(2)
+    rconsolewarn('discord.gg/pVQUgrWP')
     wait(5)
     rconsoleclear()
+    rconsolename('discord.gg/pVQUgrWP')
+    
 end
