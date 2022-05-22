133f644d633c5448b46c106c1068319837d267fb
Owner Of Nano <87079094+muzrblx@users.noreply.github.com>
2022-05-22 12:04:45 -0400

    Create 4PHLA.lua

---
+++ Hub/Loaders/4PHLA.lua
@@ -0,0 +1,22 @@
+local old = rconsoleprint;
+
+getgenv().rconsoleprint = function(...)
+   local args = {...}
+   local string = ""
+   for i,k in pairs(args) do
+       string = string .. " " .. k
+   end
+   old(string:sub(2, string:len()).."\n")
+end;
+
+rconsoleclear()
+
+rconsolename(' 4PHLA | Loader Menu ')
+
+rconsoleprint('Thanks for choosing 4PHLA | By clicking Enter you agree that if your account gets banned its your fault')
+
+if rconsoleinput() then
+    rconsolewarn('Loading in 5 seconds')
+    wait(5)
+    rconsoleclear()
+end
